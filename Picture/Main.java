50cd1ab83a68ae8185f6051af14a677a96467802
Griffin D <115186776+Griffinxaxa@users.noreply.github.com>
2024-06-22 14:00:12 -0400

    Main.java

---
+++ Picture/Main.java
@@ -0,0 +1,72 @@
+public class Main 
+{
+  /* Main method for testing */
+  public static void main(String[] args) 
+  {
+    // zeroBlue() test
+    // Picture pic = new Picture("pics/beach.jpg");
+    // pic.zeroBlue();
+    // pic.write("output.jpg");
+
+     // keepOnlyBlue() test
+       // Picture pic = new Picture("pics/beach.jpg");
+       // pic.keepOnlyBlue();
+       // pic.write("output.jpg");
+      
+
+    // negate() test
+      // Picture pic = new Picture("pics/puppies.jpg");
+      // pic.negate();
+      // pic.write("output.jpg");
+
+    
+    // grayscale() test
+        // Picture pic = new Picture("pics/puppies.jpg");
+        // pic.greyscale();
+        // pic.write("output.jpg");
+
+    
+    // fixUnderwater() test
+     // Picture pic = new Picture("pics/water.jpg");
+     // pic.fixUnderwater();
+     // pic.write("output.jpg");
+
+    
+    // mirrorHorizontal() test
+    // Picture pic = new Picture("pics/redMotorcycle.jpg");
+    // pic.mirrorHorizontal();
+    // pic.write("output.jpg");
+
+    
+    // mirrorVerticalRightToLeft() test
+     // Picture pic = new Picture("pics/caterpillar.jpg");
+     // pic.mirrorVerticalRightToLeft();
+     // pic.write("output.jpg");
+
+    
+    // mirrorHorizontalBotToTop() test
+     // Picture pic = new Picture("pics/redMotorcycle.jpg");
+     // pic.mirrorHorizontalBotToTop();
+     // pic.write("output.jpg");
+
+    
+    // mirrorDiagonal() test
+     // Picture pic = new Picture("pics/beach.jpg");
+      //pic.mirrorDiagonal();
+      //pic.write("output.jpg");
+
+    
+  }
+}
+
+/*
+  This is Picture Lab with all of the GUI stripped out.
+  I also simplified some things, removed some unused 
+  methods, and removed the inheritance aspect (as I used 
+  it in Unit 8 before I covered inheritance).
+
+  The pic.write("output.jpg") line creates output in the
+  output.jpg file at left.  Repl.it lets you open it
+  and view it directly so you can see what effect your
+  code had on the image.
+*/
